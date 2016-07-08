4d3b254f2ecbdfc0a284f5a63eac4984f277f610
zooeymoon1989 <kimmi-moon@163.com>
2016-07-09 01:43:47 +0800

    第五章内容
    Signed-off-by: zooeymoon1989 <kimmi-moon@163.com>

--- common/models/User.php
+++ common/models/User.php
@@ -1,11 +1,20 @@
 <?php
 namespace common\models;
 
+use backend\models\Role;
 use Yii;
 use yii\base\NotSupportedException;
 use yii\behaviors\TimestampBehavior;
+use yii\bootstrap\Html;
 use yii\db\ActiveRecord;
+use yii\db\Expression;
+//use yii\debug\models\search\Profile;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Url;
 use yii\web\IdentityInterface;
+use backend\models\Status;
+use backend\models\UserType;
+use frontend\models\Profile;
 
 /**
  * User model
@@ -23,8 +32,7 @@ use yii\web\IdentityInterface;
  */
 class User extends ActiveRecord implements IdentityInterface
 {
-    const STATUS_DELETED = 0;
-    const STATUS_ACTIVE = 10;
+    const STATUS_ACTIVE = 1;
 
 
     /**
@@ -35,6 +43,25 @@ class User extends ActiveRecord implements IdentityInterface
         return '{{%user}}';
     }
 
+
+
+    /* Your model attribute labels */
+    public function attributeLabels()
+    {
+        return [
+            /* Your other attribute labels */
+            'roleName' => Yii::t('app', 'Role'),
+            'statusName' => Yii::t('app', 'Status'),
+            'profileId' => Yii::t('app', 'Profile'),
+            'profileLink' => Yii::t('app', 'Profile'),
+            'userLink' => Yii::t('app', 'User'),
+            'username' => Yii::t('app', 'User'),
+            'userTypeName' => Yii::t('app', 'User Type'),
+            'userTypeId' => Yii::t('app', 'User Type'),
+            'userIdLink' => Yii::t('app', 'ID'),
+        ];
+    }
+
     /**
      * @inheritdoc
      */
@@ -42,6 +69,14 @@ class User extends ActiveRecord implements IdentityInterface
     {
         return [
             TimestampBehavior::className(),
+            'timestamp'=>[
+                'class'=>'yii\behaviors\TimestampBehavior',
+                'attributes'=>[
+                    ActiveRecord::EVENT_BEFORE_INSERT=>['created_at','updated_at'],
+                    ActiveRecord::EVENT_BEFORE_UPDATE=>['updated_at']
+                ],
+                'value'=>new Expression('NOW()')
+            ],
         ];
     }
 
@@ -51,17 +86,31 @@ class User extends ActiveRecord implements IdentityInterface
     public function rules()
     {
         return [
-            ['status', 'default', 'value' => self::STATUS_ACTIVE],
-            ['status', 'in', 'range' => [self::STATUS_ACTIVE, self::STATUS_DELETED]],
+            ['status_id', 'default', 'value' => self::STATUS_ACTIVE],
+            [['status_id'],'in','range'=>array_keys($this->getStatusList())],
+            ['role_id','default','value'=>1],
+            [['role_id'],'in', 'range'=>array_keys($this->getRoleList())],
+            ['user_type_id','default','value'=>1],
+            [['user_type_id'],'in', 'range'=>array_keys($this->getUserTypeList())],
+            ['username','filter','filter'=>'trim'],
+            ['username','required'],
+            ['username','unique'],
+            ['username','string','min'=>2,'max'=>255],
+            ['email','filter','filter'=>'trim'],
+            ['email','required'],
+            ['email','email'],
+            ['email','unique']
         ];
     }
+
+
 
     /**
      * @inheritdoc
      */
     public static function findIdentity($id)
     {
-        return static::findOne(['id' => $id, 'status' => self::STATUS_ACTIVE]);
+        return static::findOne(['id' => $id, 'status_id' => self::STATUS_ACTIVE]);
     }
 
     /**
@@ -80,7 +129,7 @@ class User extends ActiveRecord implements IdentityInterface
      */
     public static function findByUsername($username)
     {
-        return static::findOne(['username' => $username, 'status' => self::STATUS_ACTIVE]);
+        return static::findOne(['username' => $username, 'status_id' => self::STATUS_ACTIVE]);
     }
 
     /**
@@ -97,7 +146,7 @@ class User extends ActiveRecord implements IdentityInterface
 
         return static::findOne([
             'password_reset_token' => $token,
-            'status' => self::STATUS_ACTIVE,
+            'status_id' => self::STATUS_ACTIVE,
         ]);
     }
 
@@ -186,4 +235,77 @@ class User extends ActiveRecord implements IdentityInterface
     {
         $this->password_reset_token = null;
     }
+
+    public function getProfile(){
+        return $this->hasOne(Profile::className(),['user_id'=>'id']);
+    }
+
+
+    public function getRole(){
+        return $this->hasOne(Role::className(),['id'=>'role_id']);
+    }
+
+    public function getRoleName(){
+        return $this->role?$this->role->role_name : '- no role -';
+    }
+
+    public static function getRoleList(){
+        $droptions = Role::find()->asArray()->all();
+        return ArrayHelper::map($droptions,'id','role_name');
+    }
+
+    public function getStatus(){
+        return $this->hasOne(Status::className(),['id'=>'status_id']);
+    }
+
+    public function getStatusName(){
+        return $this->status?$this->status->status_name:'- no status -';
+    }
+
+    public static function getStatusList(){
+        $droptions = Status::find()->asArray()->all();
+        return ArrayHelper::map($droptions,'id','status_name');
+    }
+
+    public function getUserType(){
+        return $this->hasOne(UserType::className(),['id'=>'user_type_id']);
+    }
+
+    public function getUserTypeName(){
+        return $this->userType?$this->userType->user_type_name:'- no user type -';
+    }
+
+    public static function getUserTypeList(){
+        $droptions = UserType::find()->asArray()->all();
+        return ArrayHelper::map($droptions,'id','user_type_name');
+    }
+
+    public function getUserTypeId()
+    {
+        return $this->userType ? $this->userType->id : 'none';
+    }
+
+    public function getProfileId(){
+        return $this->profile ? $this->profile->id:'none';
+    }
+
+    
+    public function getProfileLink(){
+        $url = Url::to(['profile/update',['id'=>$this->profileId]]);
+        $options = [];
+        return Html::a($this->profile?'profile':'none',$url,$options);
+    }
+
+    public function getUserIdLink(){
+        $url = Url::to(['user/update','id'=>$this->id]);
+        $options = [];
+        return Html::a($this->id,$url,$options);
+    }
+
+    public function getUserLink(){
+        $url = Url::to(['user/view','id'=>$this->id]);
+        $options = [];
+        return Html::a($this->id,$url,$options);
+    }
+    
 }
---
+++ frontend/models/Profile.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace frontend\models;
+
+use Yii;
+use yii\db\ActiveRecord;
+use common\models\User;
+use yii\helpers\Url;
+use yii\helpers\Html;
+use yii\helpers\ArrayHelper;
+use yii\db\Expression;
+
+/**
+ * This is the model class for table "profile".
+ *
+ * @property string $id
+ * @property string $user_id
+ * @property string $first_name
+ * @property string $last_name
+ * @property string $birthday
+ * @property string $gender_id
+ * @property string $created_at
+ * @property string $updated_at
+ *
+ * @property Gender $gender
+ */
+class Profile extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'profile';
+    }
+
+
+    public function behaviors()
+    {
+        return [
+            'timestamp' => [
+                'class' => 'yii\behaviors\TimestampBehavior',
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => ['created_at', 'updated_at'],
+                    ActiveRecord::EVENT_BEFORE_UPDATE => ['updated_at'],
+                ],
+                'value' => new Expression('NOW()'),
+            ],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['user_id', 'gender_id'], 'required'],
+            [['user_id', 'gender_id'], 'integer'],
+            [['birthday', 'created_at', 'updated_at'], 'safe'],
+            [['first_name', 'last_name'], 'string', 'max' => 60],
+            [['birthday'], 'date', 'format'=>'Y-m-d'],
+            [['birthday', 'created_at', 'updated_at'], 'safe'],
+            [['gender_id'],'in', 'range'=>array_keys($this->getGenderList())],
+            [['gender_id'], 'exist', 'skipOnError' => true, 'targetClass' => Gender::className(), 'targetAttribute' => ['gender_id' => 'id']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'user_id' => 'User ID',
+            'first_name' => 'First Name',
+            'last_name' => 'Last Name',
+            'birthday' => 'Birthday',
+            'gender_id' => 'Gender ID',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+            'genderName' => Yii::t('app', 'Gender'),
+            'userLink' => Yii::t('app', 'User'),
+            'profileIdLink' => Yii::t('app', 'Profile'),
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getGender()
+    {
+        return $this->hasOne(Gender::className(), ['id' => 'gender_id']);
+    }
+
+    public function getGenderName()
+    {
+        return $this->gender->gender_name;
+    }
+
+    public static function getGenderList()
+    {
+        $droptions = Gender::find()->asArray()->all();
+        return ArrayHelper::map($droptions, 'id', 'gender_name');
+    }
+
+    public function getUser(){
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
+
+    public function getUserName()
+    {
+        return $this->user->username;
+    }
+
+    public function getUserId()
+    {
+        return $this->user ? $this->user->id : 'none';
+    }
+
+    /**
+     * @getUserLink
+     */
+    public function getUserLink()
+    {
+        $url = Url::to(['user/view','id'=>$this->user_id]);
+        $options = [];
+        return Html::a($this->getUserName(),$url,$options);
+    }
+
+    public function getProfileLink(){
+        $url = Url::to(['profile/update','id'=>$this->id]);
+        $options = [];
+        return Html::a($this->id,$url,$options);
+    }
+
+
+}

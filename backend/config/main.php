acb5fa84ce6ecdc9e87bd3cbad2b18c433242ef3
zooeymoon <kimmi-moon@163.com>
2016-10-08 00:53:29 +0800

    add htaccess

--- backend/config/main.php
+++ backend/config/main.php
@@ -29,14 +29,17 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
-        'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
-            'rules' => [
-            ],
-        ],
-        */
+//        'urlManager' => [
+//            'class'=>'yii\web\UrlManager',
+//            'enablePrettyUrl' => true,
+//            'showScriptName' => false,
+//            'rules' => [
+//                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+//                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+//                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+//                '<controller:\w+\-\w+>/<id:\d+>' => '<controller>/view',
+//            ],
+//        ],
     ],
     'params' => $params,
 ];
